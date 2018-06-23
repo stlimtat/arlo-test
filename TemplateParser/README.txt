83b0a77bf7755857c8561832281ce55a2aebdee3
Lim Swee Tat <sweetat.lim@finantix.com>
2018-06-23 11:49:13 +0800

    Initial copy with the zip from Arlo

---
+++ TemplateParser/README.txt
@@ -0,0 +1,6 @@
+﻿TemplateParser.csproj
+---------------------
+
+- Write your implementation in TemplateEngineImpl.cs so that it passes all of the tests in TemplateParser.Test
+- To test your implementation, set TemplateParser.Test as the startup project, and use F5 to run its Main method.
+- You may also use the integrated Test Explorer to run the tests within the IDE.
---
+++ TemplateParser/TemplateEngineImpl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateParser {
+    public class TemplateEngineImpl : ITemplateEngine {
+
+        /// <summary>
+        /// Applies the specified datasource to a string template, and returns a result string
+        /// with substituted values.
+        /// </summary>
+        public string Apply(string template, object dataSource) {
+            //TODO: Write your implementation here that passes all tests in TemplateParser.Test project            
+            throw new NotImplementedException();
+        }
+    }
+}
