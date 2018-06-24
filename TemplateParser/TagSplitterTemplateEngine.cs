6ec6356021b80375b188fc86ca24eeef84e5c344
st_lim <st_lim@stlim.net>
2018-06-24 03:49:34 -0700

    Adding a TagSplitterTemplateEngine that can be called recursively with different tag parser sets
    Taking most of the code out of TemplateEngineImpl

---
+++ TemplateParser/TagSplitterTemplateEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateParser
+{
+    class TagSplitterTemplateEngine : ITemplateEngine
+    {
+        private List<IMyParser> tagMyParsers;
+
+        TagSplitterTemplateEngine(List<IMyParser> tagMyParsers)
+        {
+            this.tagMyParsers = tagMyParsers;
+        }
+
+        string ITemplateEngine.Apply(string template, object dataSource)
+        {
+            StringBuilder result = new StringBuilder();
+            string currString = template;
+            // Search for all occurances of tokens
+            // start from -1 instead of 0 since 0 can be a valid start of a tag
+            for (int openBracketIndex = -1; (openBracketIndex = currString.IndexOf('[')) > -1;)
+            {
+                result.Append(currString.Substring(0, openBracketIndex - 1));
+                // Determine the type of parser
+                int closeBrackerIndex = currString.IndexOf(']', openBracketIndex);
+                int tagLength = closeBrackerIndex - openBracketIndex - 1;
+                string currTag = currString.Substring(openBracketIndex + 1, tagLength);
+                // with parser
+                foreach (IMyParser myParser in tagMyParsers)
+                {
+                    if (myParser.IdentifyParser(currTag))
+                    {
+                        string tag_value = myParser.Apply(currTag, dataSource);
+                        result.Append(tag_value);
+                        currString = myParser.Truncate(currString, closeBrackerIndex);
+                    }
+                }
+                if (currString.Length > 0)
+                {
+                    result.Append(currString);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- TemplateParser/TemplateEngineImpl.cs
+++ TemplateParser/TemplateEngineImpl.cs
@@ -6,15 +6,17 @@ using System.Text.RegularExpressions;
 
 namespace TemplateParser {
     public class TemplateEngineImpl : ITemplateEngine {
+        List<IMyParser> myTagParsers;
 
-        List<IMyParser> tag_parsers;
-
-        public TemplateEngineImpl()
+        TemplateEngineImpl()
         {
-            // Normally, I will use dependency injection to inject in the behaviour I want
-            // But in this case, I am just gonna take a shortcut and just inject it in the
-            // constructor
-            tag_parsers = new List<IMyParser>();
+            // Usually I will use dependency injection to run this initialization
+            // But in this case, I leave it to the constructor
+            this.myTagParsers = new List<IMyParser>();
+            // the With tag parser
+            // the composite tag parser
+            // the dataSource simple tag parser
+            // the C# expression language tag parser
         }
 
         /// <summary>
@@ -23,35 +25,12 @@ namespace TemplateParser {
         /// </summary>
         public string Apply(string template, object dataSource)
         {
-            StringBuilder result = new StringBuilder();
+            string result = null;
 
-            string curr_string = template;
-            // Search for all occurances of tokens
-            // start from -1 instead of 0 since 0 can be a valid start of a tag
-            for (int open_brkt_index = -1; (open_brkt_index = curr_string.IndexOf('[')) > -1;)
-            {
-                result.Append(curr_string.Substring(0, open_brkt_index - 1));
-                // Determine the type of parser
-                int close_brkt_index = curr_string.IndexOf(']', open_brkt_index);
-                int tag_length = close_brkt_index - open_brkt_index - 1;
-                string curr_tag = curr_string.Substring(open_brkt_index + 1, tag_length);
-                // with parser
-                foreach (IMyParser my_parser in tag_parsers)
-                {
-                    if (my_parser.IdentifyParser(curr_tag))
-                    {
-                        string tag_value = my_parser.Apply(curr_tag, dataSource);
-                        result.Append(tag_value);
-                        curr_string = my_parser.Truncate(curr_string, close_brkt_index);
-                    }
-                }
-                if (curr_string.Length > 0)
-                {
-                    result.Append(curr_string);
-                }
-            }
 
-            return result.ToString();
+            TagSplitterTemplateEngine tagSplitterTemplateEngine = new TagSplitterTemplateEngine(this.myTagParsers);
+
+            return result;
         }
     }
 }
